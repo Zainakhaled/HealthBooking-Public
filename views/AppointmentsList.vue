e728047f8dc75183116e59ee40ecbc25aef4411c
Zainakhaled <103534579+Zainakhaled@users.noreply.github.com>
2026-06-13 13:21:23 +0300

    Update AppointmentsList.vue

--- views/AppointmentsList.vue
+++ views/AppointmentsList.vue
@@ -59,7 +59,7 @@ export default {
   },
   methods: {
     fetchAppointments() {
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments")
+      fetch("https://o0poh36gp7.execute-api.eu-central-1.amazonaws.com/appointments")
         .then(res => res.json())
         .then(data => {
           const parsed = JSON.parse(data.body);
@@ -74,9 +74,12 @@ export default {
       console.log("appointmentId:", cleanAppointment.appointmentId);
       console.log(" appointmentId (direct):", appointment.appointmentId);
 
-      const url = `https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments/${appointment.appointmentId}`;
+      const url = `https://o0poh36gp7.execute-api.eu-central-1.amazonaws.com/appointments`;
 
-      const payload = { status: newStatus };
+      const payload = {
+      appointmentId: appointment.appointmentId,
+      status: newStatus
+      };
 
       fetch(url, {
         method: "PATCH",
