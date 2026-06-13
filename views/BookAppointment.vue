246a21095a0f116c2142daff630de40171586f0f
Zainakhaled <103534579+Zainakhaled@users.noreply.github.com>
2026-06-13 13:08:02 +0300

    Update BookAppointment.vue

--- views/BookAppointment.vue
+++ views/BookAppointment.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   mounted() {
-    fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/slots")
+    fetch("https://o0poh36gp7.execute-api.eu-central-1.amazonaws.com/slots")
       .then(res => res.json())
       .then(data => {
         const parsed = JSON.parse(data.body);
